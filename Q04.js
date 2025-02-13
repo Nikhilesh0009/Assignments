151aa2e53f46599feffd53e1f675fd78ee6b9aa2
Nikhilesh <nikhileshreddy861@gmail.com>
2025-02-13 21:55:40 +0530

    Q

--- Q04.js
+++ Q04.js
@@ -1,84 +1,48 @@
-// Online Javascript Editor for free
-// Write, Edit and Run your Javascript code using JS Online Compiler
-
-// Online Javascript Editor for free
-// Write, Edit and Run your Javascript code using JS Online Compiler
-
-
-let users=[
-   {
-       role :"admin",
-  experience: 7,
-     active :true,
-  department: "IT"
-   },
-   {
-       role :"admin",
-  experience: 3,
-     active :true,
-  department: "Finance"
-
-   },
-   {
-       role :"admin",
-  experience: 6,
-     active :true,
-  department: "Marketing"
-
-   },
-   {
-       role :"admin",
-  experience: 5,
-     active :false,
-  department: "Finance"
-
-   }
    
-//     {
-//         role :"Manager",
-//   experience: 4,
-//       active :true,
-//   department: "Marketing"
-//     },
-//      {
-//         role :"Manager",
-//   experience: 4,
-//       active :true,
-//   department: "Marketing"
-//     },
-//     {
-//         role :"Manager",
-//   experience: 4,
-//       active :true,
-//   department: "Marketing"
-//     },
-//     {
-//         role :"Manager",
-//   experience: 4,
-//       active :true,
-//   department: "Marketing"
-//     },
    
-//     {
-//         role :"user",
-//   experience: 2,
-//       active :true,
-//   department: "Support"
-//     },
-   ]
-   
-   for(i=0; i<users.length; i++){
+   function advancedUser(users){
        
-    let output=(users[i]["role"]=="admin" && users[i]["active"]==true &&users[i]["experience"]>5 &&users[i]["department"]=="IT" )? "Full IT Admin Access"
-     :(users[i]["role"]=="admin" && users[i]["active"]==true &&users[i]["experience"]>5 &&users[i]["department"]!="IT" )? "Full General Admin Access"
-     :(users[i]["role"]=="admin" && users[i]["active"]==true &&users[i]["experience"]<=5 )? "Limited Admin Access"
-     :"Admin Access Revoked"
-     
-     console.log(output)
-   }
+      //admin
+
+   let output=(users["role"]=="admin" && users["active"]==true &&users["experience"]>5 &&users["department"]=="IT" )? "Full IT Admin Access"
+    :(users["role"]=="admin" && users["active"]==true &&users["experience"]>5 &&users["department"]!="IT" )? "Full General Admin Access"
+    :(users["role"]=="admin" && users["active"]==true &&users["experience"]<=5 )? "Limited Admin Access"
+    :(users["role"]=="admin" && users["active"]==false)?"Admin Access Revoked" 
    
-   
-   
-   
-   
+    // Manager 
+    
+    :(users["role"]=="manager" && users["active"]==true &&users["experience"]>3 &&users["department"]=="Sales" )? "Full Sales Manager Access"
+    :(users["role"]=="manager" && users["active"]==true &&users["experience"]>3 &&users["department"]!="Sales" )? "Full Manager Access"
+    :(users["role"]=="manager" && users["active"]==true &&users["experience"]<=3 )? "Limited Manager Access"
+    :(users["role"]=="manager" && users["active"]==false)?"Manager Access Revoked"
+    
+    //user 
+
+    :(users["role"]=="user" &&users["department"]=="Support" )? "Priority Support Access"
+    :(users["role"]=="user"  &&users["department"]!="Support" )? "User Access"
+    :(users["role"]=="user" && users["active"]==false)?"User Access Revoked"
+    :"Invalid Role"
+    
+    return output;
+    
+  }
+  
+  let person1 = { role: "admin", experience: 7, active: true, department: "IT" };
+  
+  let person2 = { role: "manager", experience: 4, active: true, department: "Marketing" };
+  
+  let person3 = { role: "user", experience: 2, active: true, department: "Support" };
+
+  let person4 = { role: "admin", experience: 3, active: false, department: "Finance" };
+
+
+  
+  console.log(advancedUser(person1))
+  console.log(advancedUser(person2))
+  console.log(advancedUser(person3))
+  console.log(advancedUser(person4))
+
+  
+
+
    
