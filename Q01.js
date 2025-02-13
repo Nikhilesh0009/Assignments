b549a7766532e4a3d9c41754aa77e16cfd8cdf50
Nikhilesh <nikhileshreddy861@gmail.com>
2025-02-13 17:28:25 +0530

    Q01

--- Q01.js
+++ Q01.js
@@ -1,1 +1,28 @@
-console.log("holle")
+let scores=[95,50,35,65,20,82,90,36,75,28]
+
+
+let count1 = 0
+let count2=0
+
+
+for(i=0; i<scores.length; i++){
+    
+    if(scores[i]<40){
+        scores[i]+=20
+    }
+    if(scores[i]>90){
+        scores[i]=90
+    }
+    if(scores[i]>=50){
+     count1++
+    }
+    else{
+     count2++
+
+    }
+    
+}
+
+ console.log(` students passed : ${count1}`)
+  console.log(` students  Failed : ${count2}`)
+
